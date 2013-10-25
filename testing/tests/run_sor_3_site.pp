489ef1504a0aed21cf622fa3596739cb363f71f1
narmitag <neil@neila.co.uk>
2013-10-25 14:53:36 +0100

    More changes for the hosts file

--- testing/tests/run_sor_3_site.pp
+++ testing/tests/run_sor_3_site.pp
@@ -19,7 +19,7 @@
 class { 'continuent_install' :
       nodeHostName                => 'east-db1' ,
       nodeIpAddress               => "${::ipaddress}" ,
-hostsFile                  => ["${::ipaddress}  east-db1",'10.0.0.6 north-db1','10.0.0.7 north-db2','192.168.0.146 east-db2','192.168.0.147 west-db1','192.168.0.148 west-db2'],
+hostsFile                  => ["${::ipaddress},east-db1",'10.0.0.6,north-db1','10.0.0.7,north-db2','192.168.0.146,east-db2','192.168.0.147,west-db1','192.168.0.148,west-db2'],
 
       clusterData                => {
       east => { 'members' => 'east-db1,east-db2', 'connectors' => 'east-db1,east-db2', 'master' => 'east-db1' },
