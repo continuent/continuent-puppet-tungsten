2c0c48f7ac9f85649f614d7d63328d0f75c29a3b
narmitag <neil@neila.co.uk>
2013-12-23 16:45:02 +0000

    test converted to vagrant

--- testing/test_puppet_vagrant.rb
+++ testing/test_puppet_vagrant.rb
@@ -26,6 +26,8 @@ require 'tempfile'
 
 
 
+stopOnFailure=false
+
 vagrantType='vb'
 #vagrantType'ec2'
 
@@ -52,109 +54,61 @@ loop = 1
 
 #tests to run
 runTypes = Dir['tests/*.pp']
-runTypes = %w(run_std_no_mysql.pp)
+runTypes = %w(run_std_no_mysqlj.pp)
 
 runTypes.sort!
 noOfTests=runTypes.count
 puts "Test run consists of #{noOfTests} tests"
 
-#Some test require MySQL to be installed on the hosts already - simulating a pre-installed customer config
-#putting the test in this array will trigger an extra test puppet class to be called to pre-install and config mysql
-#before running the test
-#requireMysql = %w(run_sor_no_mysql.pp run_no_install_no_mysql.pp)
 
-#Some test require MySQLJ to be installed on the hosts already - simulating a pre-installed customer config
-#putting the test in this array will trigger an extra test puppet class to be called to pre-install and config mysql
-#before running the test
-#requireMysqlj     = %w(run_std_no_mysqlj.pp )
-#dontCheckCluster  = %w(run_no_install.pp run_no_install_no_mysql.pp sandbox.pp rvm.pp replicator.pp replicator_nightly.pp connector_only.pp build_ini.pp)
-#dontTerminate     = %w(1prov_setup.pp run_std_no_mysql.pp)
-#checkConnector    = %w(connector_only.pp)
-#
-#requirePerconaRepo = %w(run_no_install.pp run_no_install_no_mysql.pp)
+testNumber=1
 
+runTypes.each do |runType|
 
-    testNumber=1
+    runType=File.basename(runType)
+    runTypeShort=File.basename(runType,".*")
 
-    runTypes.each do |runType|
+    runDetails=JSON.parse(File.read("tests/#{runTypeShort}.json"))
+    pp runDetails
 
-        runType=File.basename(runType)
-        runTypeShort=File.basename(runType,".*")
+    puts "(#{testNumber}/#{noOfTests}) Running test #{runType} - #{runDetails['description']}  "
+    puts '   - calling Vagrant to launch a clean node'
+    puts "   - vagrant type = #{vagrantType}"
+    puts "   - number of nodes = #{runDetails['nodes']}"
 
-        runDetails=JSON.parse(File.read("tests/#{runTypeShort}.json"))
-        pp runDetails
+    system('vagrant destroy -f')
 
-        puts "(#{testNumber}/#{noOfTests}) Running test #{runType} - #{runDetails['description']}  "
-        puts '   - calling Vagrant to launch a clean node'
-        puts "   - vagrant type = #{vagrantType}"
-        puts "   - number of nodes = #{runDetails['nodes']}"
+    system("rm VagrantFile ; cp Vagrantfile.#{vagrantType}.#{runDetails['nodes']} VagrantFile")
+    system('vagrant up')
 
-        system('vagrant destroy -f')
+    puts '   - Installing and running puppet modules on each node'
 
-        system("rm VagrantFile ; cp Vagrantfile.#{vagrantType}.#{runDetails['nodes']} VagrantFile")
-        system('vagrant up')
+    for n in 1..runDetails['nodes']
+      puts "     - Provisioning node #{n} "
+      system ("vagrant ssh db#{n} -c 'sudo mkdir /etc/puppet/modules/continuent_install'")
+      system ("vagrant ssh db#{n} -c 'sudo cp -r /vagrant/module/* /etc/puppet/modules/continuent_install'")
 
-        puts '   - Installing and running puppet modules on each node'
+     puppetOutput = capture_stdout do
+        system "vagrant ssh db#{n} -c 'cd /vagrant/tests; sh run_test.sh #{runType} #{runDetails['pre-reqs'].join(',')}'"
+     end
 
-        for n in 1..runDetails['nodes']
-          puts "     - Provisioning node #{n} "
-          system ("vagrant ssh db#{n} -c 'sudo mkdir /etc/puppet/modules/continuent_install'")
-          system ("vagrant ssh db#{n} -c 'sudo cp -r /vagrant/module/* /etc/puppet/modules/continuent_install'")
-
-          #puppetOutput = system "vagrant ssh db#{n} -c 'cd /vagrant/tests; sh run_test.sh #{runType} #{runDetails['pre-reqs'].join(',')}'"
-
-          puppetOutput = capture_stdout do
-            system "vagrant ssh db#{n} -c 'cd /vagrant/tests; sh run_test.sh #{runType} #{runDetails['pre-reqs'].join(',')}'"
-          end
-
-          puppetError=false
-          if puppetOutput.include?('Error:')
-            puppetError=true
-            puts '>>>>>>>>>>>>>>>> PUPPET ERROR <<<<<<<<<<<<<< '
-            allTestsPassed=false
-          else
-            puts "     - Puppet Modules installed on db#{n} with no errors "
-          end
-        end
-
-
-
-
-            #sleep 20
-            #
-            #if !puppetError
-            #    if dontCheckCluster.include?(runType)
-            #
-            #      if checkConnector.include?(runType)
-            #        #output=vagrantSSH( "echo 'ls'|/opt/continuent/tungsten/tungsten-connector/bin/connector status")
-            #
-            #        #if output.include?('Tungsten Connector Service is running')
-            #        #  puts '   - connector installed ok - will terminate instance'
-            #        #else
-            #        #  puts '>>>>>>>>>>  CONNECTOR FAILED TO LAUNCH  <<<<<<<<<<<<<<<<<<<'
-            #        #  allTestsPassed=false
-            #        #end
-            #      else
-            #        puts '   - no cluster to check for this runtype - will terminate instance'
-            #      end
-            #    else
-            #
-            #      #cctrlOutput=vagrantSSH("echo 'ls'|/opt/continuent/tungsten/tungsten-manager/bin/cctrl")
-            #
-            #      #if cctrlOutput.include?('ONLINE')
-            #      #  ??
-            #      #else
-            #      #  puts '>>>>>>>>>>  CLUSTER FAILED TO LAUNCH  <<<<<<<<<<<<<<<<<<<'
-            #      #  allTestsPassed=false
-            #      #end
-            #    end
-
-        #end
-
-
-        system ('vagrant destroy -f')
-        testNumber=testNumber+1
+      puppetError=false
+      if puppetOutput.include?('Error:')
+        puppetError=true
+        puts '>>>>>>>>>>>>>>>> PUPPET ERROR <<<<<<<<<<<<<< '
+        allTestsPassed=false
+      else
+        puts "     - Puppet Modules installed on db#{n} with no errors "
+      end
     end
+
+    if stopOnFailure && !allTestsPassed
+      exit 2
+    end
+    system ('vagrant destroy -f')
+
+    testNumber=testNumber+1
+end
 
 
 if allTestsPassed
