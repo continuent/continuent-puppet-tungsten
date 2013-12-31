9dca21809bbcb9101bfdde6fae4e97e90248c7a2
narmitag <neil@neila.co.uk>
2013-12-31 10:54:36 +0000

    Fix Composite datasource installs

--- manifests/tungsten/ini.pp
+++ manifests/tungsten/ini.pp
@@ -1,7 +1,11 @@
 class continuent_install::tungsten::ini (
 ) inherits continuent_install::tungsten {
 	include continuent_install::prereq
-	
+
+    if $continuent_install::tungsten::clusterData != false {
+      $compositeInfo = getCompositeDS($continuent_install::tungsten::clusterData)
+    }
+
 	Class["continuent_install::prereq"] ->
 	file { "tungsten.ini":
 		path		=> "/etc/tungsten/tungsten.ini",
