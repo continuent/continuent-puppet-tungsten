337f59cd46d28fa444cf087047b4493229f25929
Jeff Mace <jeffmace@gmail.com>
2014-09-16 19:59:03 +0000

    Moving the epel_version fact to the percona_repo module

--- lib/facter/epel_version.rb
+++
@@ -1,8 +0,0 @@
-# epel_version.rb
-# A facter fact to determine the epel version
-
-Facter.add('epel_version') do
-  setcode do
-    Facter::Util::Resolution.exec("rpm -qa | grep epel-release | awk -F- '{print $3}'")
-  end
-end
