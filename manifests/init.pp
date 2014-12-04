f0ff7fe20862c8717cd87d4f939a2626abe2031b
narmitag <neil@neila.co.uk>
2014-12-04 10:29:15 +0000

    fix class

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,7 +34,7 @@ class tungsten (
   	  $sshPrivateCert               = $tungsten::params::defaultSSHPrivateCert,
 		
 		$installMysql										= false,
-      $mysqlOverrideOptions         = { },
+      $mysqlOverrideOptions         = {},
 
 		# Set this to true if you are not passing $clusterData
 	        # and want the /etc/tungsten/defaults.tungsten.ini file
@@ -87,7 +87,9 @@ class tungsten (
         }
       }
       if $installMysql == true {
-        include tungstenmysql
+        class {"tungsten::tungstenmysql":
+          mysqlOverrideOptions => $mysqlOverrideOptions,
+        }
       }
   } else {
     if $installMysql == true {
