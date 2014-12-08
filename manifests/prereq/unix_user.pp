2eb0cf3ec0a9347d06bc6ef29a5d90fd947e01f7
narmitag <neil@neila.co.uk>
2014-12-08 10:02:40 +0000

    swtih to echo

--- manifests/prereq/unix_user.pp
+++ manifests/prereq/unix_user.pp
@@ -78,7 +78,7 @@ class tungsten::prereq::unix_user(
   #Centos/Rhel 5 does not support /etc/sudoers.d files (CONT-147)
   if $osfamily == 'RedHat' and $operatingsystemmajrelease == 5 {
       exec { "tungsten::prereq::sudo":
-        command => "/bin/sed -i -e '$a\$systemUserName ALL=(ALL) NOPASSWD: ALL' /etc/sudoers",
+        command => "/bin/echo  '$systemUserName ALL=(ALL) NOPASSWD: ALL' >>  /etc/sudoers",
         unless => "/bin/grep $systemUserName /etc/sudoers",
         require => Package[sudo],
       }
