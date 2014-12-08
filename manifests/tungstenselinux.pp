8a98361450d12f49898bad50a51558f443b3d336
narmitag <neil@neila.co.uk>
2014-12-08 10:56:43 +0000

    remove tmp file

--- manifests/tungstenselinux.pp
+++ manifests/tungstenselinux.pp
@@ -5,14 +5,6 @@ class tungsten::tungstenselinux(
   if $disableSELinux == true {
     if $::osfamily == 'RedHat'{
         if $operatingsystemmajrelease == 5 {
-          file { 'tmp-selinux-config':
-            ensure  => file,
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0744',
-            path    => '/etc/selinux/config',
-            content => "#Dummy file"
-          }->
             class { 'selinux':
               mode => 'disabled'
             }
