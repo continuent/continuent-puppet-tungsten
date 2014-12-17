7641780536137bb92cac62e2463d8039a0ce6997
narmitag <neil@neila.co.uk>
2014-12-17 07:40:57 +0000

    Move selinux class to prereq

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
 # == Class: tungsten See README.md for documentation.
 #
 # Copyright (C) 2014 Continuent, Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License.  You may obtain
 # a copy of the License at
-# 
+#
 #         http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -22,17 +22,17 @@ class tungsten (
 		$installNTP											= $tungsten::params::installNTP,
 		$disableFirewall							  = true,
 		$disableSELinux                 = true,
-		
+
 		# This may be set to 'nightly', 'stable' or 'true
 		# If set to 'true', the stable repository will be used
 		$replicatorRepo									= false,
-		
-		#Setting this to true should only be used to support 
+
+		#Setting this to true should only be used to support
 		#testing as it's not secure unless you specify a custom private key
 		$installSSHKeys									= false,
 		  $sshPublicKey                 = $tungsten::params::defaultSSHPublicKey,
   	  $sshPrivateCert               = $tungsten::params::defaultSSHPrivateCert,
-		
+
 		$installMysql										= false,
       $overrideOptionsMysqld         = {},
       $overrideOptionsClient         = {},
@@ -44,33 +44,33 @@ class tungsten (
 	        # and want the /etc/tungsten/defaults.tungsten.ini file
 	        # to be created
 	        $writeTungstenDefaults                  = false,
-		
+
 		# Set this to 'true' or the path of a tungsten-replicator package
-		# If set to 'true', the 'tungsten-replicator' will be installed from 
+		# If set to 'true', the 'tungsten-replicator' will be installed from
 		# configured repositories.
 		$installReplicatorSoftware			= false,
 			$replicationUser							= tungsten,
 			$replicationPassword					= secret,
-			
+
 		# Set this to 'true' or the path of a continuent-tungsten package
-		# If set to 'true', the 'continuent-tungsten' will be installed from 
+		# If set to 'true', the 'continuent-tungsten' will be installed from
 		# configured repositories.
 		$installClusterSoftware					= false,
 			$clusterData									= false,
 			$appUser											= app_user,
 			$appPassword									= secret,
 			$applicationPort							= 3306,
-			
+
 		# Run the `tungsten_provision_slave` script after installing Tungsten
 		$provisionNode									= false,
 			$provisionDonor								= "autodetect",
-		
+
     #If this is set to true no setting of hostname will be done
     $skipHostConfig                 = false
 ) inherits tungsten::params {
 
 
-  class {"tungsten::tungstenselinux":
+  class {"tungsten::prereq::tungstenselinux":
     disableSELinux => $disableSELinux
   } ->
   class {"tungsten::tungstenmysql":
---
+++ manifests/prereq/tungstenselinux.pp
@@ -0,0 +1,32 @@
+# == Class: tungsten See README.md for documentation.
+#
+# Copyright (C) 2014 Continuent, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.  You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+class tungsten::prereq::tungstenselinux(
+  $disableSELinux							= true,
+) {
+
+  if $disableSELinux == true {
+    if $::osfamily == 'RedHat' and  $operatingsystemmajrelease > 5 {
+        if $::operatingsystem != 'Amazon' {
+          class { 'selinux':
+          mode => 'permissive'
+          }
+        }
+
+    }
+  }
+
+}
