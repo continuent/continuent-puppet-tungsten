c34ad856494dad7ee3cb718c015ba511e56060b2
narmitag <neil@neila.co.uk>
2015-02-20 08:08:44 +0000

    switch to use DNS resolution via SkyDNS

--- tests/centos6-cluster/start.sh
+++ tests/centos6-cluster/start.sh
@@ -2,11 +2,5 @@ puppet apply /mnt/base.pp --modulepath=/mnt/modules
 
 service sshd start
 service mysql start
-hc=$(cat /etc/hosts | grep 172 | grep -v `hostname`|wc -l)
-if [ "$hc" -gt "0" ]; then
-  for h in $(cat /etc/hosts | grep 172 | grep -v `hostname`| cut -f2); do
-    sudo -u tungsten scp -o StrictHostKeyChecking=no /etc/hosts $h:/tmp/hosts
-    sudo -u tungsten ssh $h "sudo cp /tmp/hosts /etc"
-   done
-fi
+
 rpm -i /mnt/rpm/ct.rpm
--- tests/u14.04-cluster/start.sh
+++ tests/u14.04-cluster/start.sh
@@ -2,12 +2,5 @@ puppet apply /mnt/base.pp --modulepath=/mnt/modules
 
 service ssh start
 service mysql start
-hc=$(cat /etc/hosts | grep 172 | grep -v `hostname`|wc -l)
-if [ "$hc" -gt "0" ]; then
-  for h in $(cat /etc/hosts | grep 172 | grep -v `hostname`| cut -f2); do
-    sudo -u tungsten scp -o StrictHostKeyChecking=no /etc/hosts $h:/tmp/hosts
-    sudo -u tungsten ssh $h "sudo cp /tmp/hosts /etc"
-   done
-fi
 
 dpkg -i /mnt/rpm/ct.deb
