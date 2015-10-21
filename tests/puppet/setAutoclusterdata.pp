6f185829f989b9ec8fd618b069531266533c097d
narmitag <neil@neila.co.uk>
2015-10-21 16:14:17 +0100

    add new tests

--- tests/puppet/setAutoclusterdata.pp
+++ tests/puppet/setAutoclusterdata.pp
@@ -9,4 +9,4 @@ $clusterData = {
 class { 'tungsten': installSSHKeys => true,
         installMysql=> true,
         disableFirewall=> false, skipHostConfig=> true ,mySQLSetAutoIncrement=>true,clusterData=>$clusterData,docker => true,
-        xtraBackupPackage=>'/mnt/xtrabackup/percona-xtrabackup-2.2.12-1.el6.x86_64.rpm'    }
+        xtraBackupPackage=>$download    }
